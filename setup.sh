4f1d10587602ff7708d945424ba8c843809eb59f
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-03-02 16:06:57 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,25 @@
+!/bin/bash
+# Install AmiRock
+# B.Titze 2021
+
+BLACK='\033[0;39m'
+BLUE='\033[1;34m'
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+GREY='\033[1;30m'
+
+chsh -s /bin/bash
+sudo chsh -s /bin/bash
+
+sudo apt-get install -y wget
+export DISTRO=buster-stable
+wget -O - apt.radxa.com/$DISTRO/public.key | sudo apt-key add -
+sudo apt-get update
+
+  sudo apt install -y dialog mc zip unzip wget
+  sudo apt install -y gparted vlc ntfs-3g
+  sudo apt install -y make g++
+
+  sudo apt install -y nemo 
+  sudo apt install -y base-devel sdl2 sdl2_ttf sdl2_image flac mpg123 libmpeg2 
+  sudo apt install -y libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev libserialport-dev
