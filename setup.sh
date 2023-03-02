657629949560e7caad711dac4862952b4970df43
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-03-02 17:05:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -62,7 +62,7 @@ systemctl start fan-control
       sudo rm -rf /usr/share/themes/Default/xfwm4/
       sudo cp -rf /usr/share/themes/Amiga3.x_hidpi/* /usr/share/themes/Default/xfwm4/
       sudo cp -rf $HOME/AmiRock/config/rpd-wallpaper/Commodore/* /usr/share/backgrounds/
-    
+      cp -rf $HOME/AmiRock/config/xfce4/* $HOME/.config/xfce4/
    
      
       
