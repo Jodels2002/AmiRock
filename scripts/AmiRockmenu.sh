85379670de03c6f82979fde5b142c8ed93dc62a9
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-03-04 13:31:44 +0100

    Update AmiRockmenu.sh

--- scripts/AmiRockmenu.sh
+++ scripts/AmiRockmenu.sh
@@ -168,7 +168,7 @@ case $CHOICE in
 	    sudo chmod -R 777 /usr/share/plymouth/
 	    mkdir $HOME/Amiga/
             cp -rf $HOME/Amiga/amiberry  $HOME/Amiga/amiberry_old
-            cp -rf $HOME/amiberry/*  $HOME/Amiga/
+            cp -rf $HOME/amiberry/*  $HOME/Amiga/amiberry_dev
              sudo rm -rf $HOME/amiberry
 	     sudo rm -rf $HOME/Amiga/abr
 	     sudo rm -rf $HOME/Amiga/cmake
