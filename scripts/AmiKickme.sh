f665557e94752218109398f38480ab0175f93947
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-03-07 12:04:33 +0100

    Update AmiKickme.sh

--- scripts/AmiKickme.sh
+++ scripts/AmiKickme.sh
@@ -15,9 +15,6 @@
       sudo chmod -R 777 /home/$USER/AmiRock
       sudo chmod -R 777 /opt/Backup/
       sudo cp -R /home/$USER/AmiRock/scripts/* /usr/local/bin
-      cp -R /opt/AmiRock/scripts/.ba* /home/$USER/
-      
-    
       sudo rm -rf /usr/share/applications/*Ami*
       sudo cp -R /home/$USER/AmiRock/config/Desktop/* /usr/share/applications/
       sudo cp -rf $HOME/AmiRock/config/Logo/* /opt/AmiRock/config/
@@ -32,12 +29,6 @@
       
     
            
-      if [ ! -d /home/$USER/.config/autostart/ ]; then
-      echo " Create Autostart "
-      mkdir /home/$USER/.config/autostart/
-      fi
-      #sudo cp -rf $HOME/AmiRock/config/Keyboard.desktop /home/$USER/.config/autostart/
-      	
       clear
       toilet "AmiRock-OS" --metal
       echo -e "$GREY AmiRock-OS ROM Operating System and Libraries" 
@@ -96,44 +87,7 @@ clear
       echo " "
       echo " " 
 fi
-       
-
-
- if [ ! -d /boot/dietpi/func/ ]; then
-      sudo cp -R $HOME/AmiRock/config/dietpi-banner /boot/dietpi/func/dietpi-banner
-      fi
-	
-
-if [ -f /home/$USER/Desktop/AmigaForever9Plus.msi ]; then
-AFimport.sh
-fi
- 
-  if [ ! -f /home/$USER/Amiga/kickstarts/amiga-os-310-a1200.rom ]; then
-   sudo cp -R /home/$USER/RetroPie/BIOS/MegaAGS-Kickstart.rom /home/$USER/Amiga/kickstarts/kick31a1200.rom
-  fi
    
-   if [ -f /home/$USER/Amiga/kickstarts/kick31a1200.rom ]; then
-       
-    
- 
-      if [ ! -f /home/$USER/Amiga/dir/System_ADVSP/System/T/CWB3.pac ]; then
-      install_ClassicWB.sh
-      fi
-     
-  
-   
-   else
-   
-   clear
-      toilet "AmiRock-OS" --metal
-      echo -e "$GREY AmiRock-OS ROM Operating System and Libraries" 
-      echo " Version V2.0 2020-2021 AmiRock-OS "
-      echo " No Rights Reserved.  "
-      echo " "
-      echo -e "$BLUE"
-      echo " "
-    
-   fi
 
 if [ ! -f /home/$USER/Amiga/data/amiberry_dev.png ]; then
       sudo cp -R $HOME/AmiRock/Amiga/amiberry_dev.png /home/$USER/Amiga/data/
@@ -144,68 +98,6 @@ if [ ! -f /home/$USER/Amiga/data/amiberry_dev.png ]; then
       #update.sh
      sudo cp -R $HOME/AmiRock/config/fs-uae/* /home/$USER/FS-UAE/Configurations/
  fi
- 
- if [ -d /home/$USER/pimiga/ ]; then
-      #update.sh
-     sudo mv /home/$USER/pimiga/ /home/$USER/pimiga3
-    
-fi    
-if [ -d /home/$USER/pimiga3/ ]; then
-     
-      sudo cp  -rf  $HOME/AmiRock/config/Pimiga3.desktop /home/$USER/Desktop/
-      sudo cp  -rf  $HOME/AmiRock/config/Pimiga3.desktop /usr/share/applications/
-      sudo cp  -rf  $HOME/AmiRock/config/config/Pimiga3.uae /home/$USER/Amiga/conf/
-      sudo cp  -rf  "$HOME/AmiRock/Amiga/screenmode.prefs" /home/$USER/pimiga3/disks/System/Prefs/Env-Archive/Sys/screenmode.prefs
-      #sudo chmod -R 777 /home/$USER/pimiga15/disks/System/
-      clear
-      toilet "AmiRock-OS" --metal
-      echo -e "$GREY AmiRock-OS ROM Operating System and Libraries" 
-      echo " Version V2.0 2020-2021 AmiRock-OS "
-      echo " No Rights Reserved.  "
-      echo " "
-fi     
-
-if [ -d /home/$USER/pimiga/ ]; then
-      #update.sh
-     sudo mv /home/$USER/pimiga/ /home/$USER/pimiga15
-    
-fi
-if [ -d /home/$USER/pimiga15/ ]; then
-     
-      sudo cp  -rf  $HOME/AmiRock/config/Pimiga1.5.desktop /home/$USER/Desktop/
-      sudo cp  -rf  $HOME/AmiRock/config/Pimiga1.5.desktop /usr/share/applications/
-      sudo cp  -rf  $HOME/AmiRock/config/config/Pimiga15.uae /home/$USER/Amiga/conf/
-      sudo cp  -rf  "$HOME/AmiRock/Amiga/screenmode.prefs" /home/$USER/pimiga15/disks/System/Prefs/Env-Archive/Sys/screenmode.prefs
-      #sudo chmod -R 777 /home/$USER/pimiga15/disks/System/
-      clear
-      toilet "AmiRock-OS" --metal
-      echo -e "$GREY AmiRock-OS ROM Operating System and Libraries" 
-      echo " Version V2.0 2020-2021 AmiRock-OS "
-      echo " No Rights Reserved.  "
-      echo " "
-fi
-if [ -d /home/$USER/pimiga2/ ]; then
-      
-      sudo cp  -rf  $HOME/AmiRock/config/Pimiga20.desktop /home/$USER/Desktop/
-      sudo cp  -rf  $HOME/AmiRock/config/Pimiga20.desktop /usr/share/applications/
-      sudo cp  -rf  $HOME/AmiRock/config/Pimiga2.uae /home/$USER/Amiga/conf/
-      sudo cp  -rf  $HOME/AmiRock/configPimiga2.uae /home/$USER/Amiga/conf/
-      sudo cp  -rf  $HOME/AmiRock/config/PimigaSS.uae /home/$USER/Amiga/conf/
-      sudo cp  -rf  "$HOME/AmiRock/config/Games Launcher.uae" /home/$USER/Amiga/conf/
-      sudo cp  -rf  "$HOME/AmiRock/Amiga/screenmode.prefs" /home/$USER/pimiga2/disks/System/Prefs/Env-Archive/Sys/screenmode.prefs
-      #sudo chmod -R 777 /home/$USER/pimiga2/disks/System/
-      cd /home/$USER/pimiga2/disks/
-      sudo cp  -rf  $HOME/AmiRock/Amiga/pm/System.zip /home/$USER/pimiga2/disks/
-      unzip -o /home/$USER/pimiga2/disks/System.zip
-      rm -rvf /home/$USER/pimiga2/disks/System.zip
-      clear
-      toilet "AmiRock-OS" --metal
-      echo -e "$GREY AmiRock-OS ROM Operating System and Libraries" 
-      echo " Version V2.0 2020-2021 AmiRock-OS "
-      echo " No Rights Reserved.  "
-      echo " "
-    
-fi   
 
 
 if [ -d /home/$USER/Amiga/dir/System_P96/ ]; then
@@ -221,50 +113,8 @@ fi
 
    
     
-   cp -rf  $HOME/Amiga/data/amiberry_dev.png /usr/share/applications/
-   if [ ! -f /opt/Backup/amiberry ]; then    
-	    
-	    cd
-            sudo rm -rf amiberry
-            git clone https://github.com/midwan/amiberry
-            cd amiberry
-	    	   
+   sudo cp -rf  $HOME/Amiga/data/amiberry_dev.png /usr/share/applications/
 
-        	clear
-      		toilet "AmiRock-OS" --metal
-      		echo " "
-      		echo " "
-      		echo "Raspberry Pi OS 32 bit is running... "
-      		
-      		make -j4 PLATFORM=rpi3
-   	    
-       
-            sudo chmod -R 777 /home/$USER/amiberry
-            cp -rf /home/$USER/Amiga/amiberry  /home/$USER/Amiga/amiberry_old
-            cp -rf /home/$USER/amiberry/amiberry  /home/$USER/Amiga/amiberry
-	   
-	    
-	  
-	    cp -rf  /home/$USER/Amiga/amiberry /opt/Backup/
-            cp -rf  /home/$USER/Amiga/amiberry_old /opt/Backup/
-	
-            cd
-            sudo rm -rf amiberry
-   
-       fi 
-	    
-           
-            sudo chmod -R 777 /home/$USER/amiberry
-            cp -rf /home/$USER/amiberry/amiberry  /home/$USER/Amiga/amiberry_dev
-	    
-	    # Backup
-	  
-	 
-	    
-            cd
-            sudo rm -rf amiberry
-      
-       
       
    
             if [ ! -f /home/$USER/Amiga/amiberry_old ]; then
@@ -284,17 +134,16 @@ fi
       echo " "
       echo " "
       echo "  First installation "
-      echo " " 
+     echo -e "$RED "
 
       echo "1>Please note that the Kickroms and Workbench files are still under copyright!  "
       echo "1>CLI"
       echo "1>	So only use this image if you own the original Amigas, Amiga Forever."
       echo "1>CLI: "
+      echo -e "$BLACK "
       echo "1>                  Greetings your´s "
       echo "1>Assign >NIL:      Bernd Titze"
-      echo " "
-      echo " " 
-      echo "	LOADWB ...  :-)"
+
       sleep 4s
       echo " "
       echo " " 
@@ -403,9 +252,9 @@ fi
       sudo chmod -R 777 /usr/local/bin/
       sudo chmod -R 777 /home/$USER/.config/
       sudo chmod -R 777 /home/$USER/.local/
-      sudo chmod -R 777 /home/$USER/Amiga/
+      sudo chmod -R 777 /opt/Amiga/
       sudo chmod -R 777 /home/$USER/Desktop/
-      sudo chmod -R 777 /usr/share/applications/
+      #sudo chmod -R 777 /usr/share/applications/
       sudo rm -rf ~/AmiRock
   
 cd
