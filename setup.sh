916ab77977393b8a8a747a5f5f24bebcbbc5e12d
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-12 09:08:32 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ RED='\033[1;31m'
 GREY='\033[1;30m'
 
 sudo rm -rf /home/$USER/.cache/*
-sudo rm -rf /tmp/
+sudo rm -rf /tmp/*
 
 export DISTRO=focal-stable
 wget -O - apt.radxa.com/$DISTRO/public.key | sudo apt-key add -
