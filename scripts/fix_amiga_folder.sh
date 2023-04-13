d3116ceff7a16a16c967d4a5e733d691372f7e75
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-13 07:49:11 +0200

    Update fix_amiga_folder.sh

--- scripts/fix_amiga_folder.sh
+++ scripts/fix_amiga_folder.sh
@@ -22,7 +22,7 @@ if [ ! -d /opt/Amiga/data/ ]; then
 	     sudo rm -rf /opt/Amiga
        sudo mkdir /opt/
 	     sudo mkdir /opt/Amiga
-	     sudo chmod -R 777 /opt/
+	    
 	     #sudo ln -s /opt/Amiga/ Amiga
              unzip -u  /opt/AmiRock/Amiga/Amiga.zip
        clear
