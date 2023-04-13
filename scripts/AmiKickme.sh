dd3734858ea3a24882faad9d50560c8693b32452
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-13 12:10:41 +0200

    Update AmiKickme.sh

--- scripts/AmiKickme.sh
+++ scripts/AmiKickme.sh
@@ -305,12 +305,12 @@ fi
       echo "Amiga"
       echo " "
 
-	      sudo chmod -R 777 /usr/share/plymouth
-	      sudo rm -rf /usr/share/plymouth/themes/armbian/watermark.png
-	      sudo cp -rf /opt/AmiRock/config/Logo/Amiga-Logo.png /usr/share/plymouth/themes/armbian/watermark.png
-	      sudo cp -rf /opt/AmiRock/config/Logo/Amiga-Logo.png /usr/share/plymouth/ubuntu-logo.png
-  	      sudo cp -rf /opt/AmiRock/config/plymouth/AmigaKickstart /usr/share/plymouth/themes/
-  	      sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/armbian/armbian.plymouth 501
+	     sudo chmod -R 777 /usr/share/plymouth/
+	 sudo rm -rf /usr/share/plymouth/themes/spinner/watermark.png
+	 sudo cp -rf /opt/AmiRock/config/Logo/Amiga-Logo.png /usr/share/plymouth/themes/spinner/watermark.png
+	 sudo cp -rf /opt/AmiRock/config/Logo/Amiga-Logo.png /usr/share/plymouth/ubuntu-logo.png
+  	sudo cp -rf /opt/AmiRock/config/plymouth/AmigaKickstart /usr/share/plymouth/themes/
+  	sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/spinner/spinner.plymouth 500
   	
    clear
       toilet "Afterburner" --metal
