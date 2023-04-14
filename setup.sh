44c666d93c99856007a441f5aa27a2053f87d7ca
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-14 07:23:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -251,6 +251,11 @@ cd
       sudo cp -rf /usr/share/themes/Amiga3.x_hidpi/* /usr/share/themes/Default/xfwm4/
       sudo cp -rf $HOME/AmiRock/config/rpd-wallpaper/Commodore/* /usr/share/backgrounds/
 cd
+
+      else
+      
+      sudo rm -rf $HOME/.config/dconf/
+      sudo cp -rf $HOME/AmiRock/config/user $HOME/.config/dconf/
             
 fi
 
@@ -282,12 +287,13 @@ if [  -d /usr/lib/armbian/ ]; then
 	 sudo apt purge nautilus nautilus-action -y
 	 sudo apt install xfce4-terminal -y
 	 sudo apt purge terminator -y
-	 
+	
 	 cp -rf /home/$USER/AmiRock/scripts/bashrc /home/$USER/.bashrc
 	 sudo usermod -a -G root rock
-	 sudo rm -rf $HOME/.config/
-         unzip -u  $HOME/AmiRock/config/afconfig.zip
-	
+	 #sudo rm -rf $HOME/.config/
+         #unzip -u  $HOME/AmiRock/config/afconfig.zip
+	 sudo rm -rf $HOME/.config/dconf/
+         sudo cp -rf $HOME/AmiRock/config/user $HOME/.config/dconf/
 	 sudo apt-get autoremove -y
 	 
 	 sudo chmod -R 775 /usr/share/plymouth/
