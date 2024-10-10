e6b3e8ebcafe7cab5f98ced1dd0c5c34508f39f0
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2024-10-10 15:54:03 +0200

    Update AmiRockmenu.sh

--- scripts/AmiRockmenu.sh
+++ scripts/AmiRockmenu.sh
@@ -80,14 +80,20 @@ case $CHOICE in
              #************************************************  Install Retropie    **************************************
             clear
                 cd
+  
 
-		git clone --depth=1 https://github.com/Jodels2002/RetroPie_Rock5b.git
+		rm -rf ~/RetroPie-Setup
+		sudo apt install -y p7zip-full
+		cd ~
+		git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
+		sudo chmod -R 777 ~/RetroPie-Setup
+		cp -f -R /opt/AmiRock/retropie/system.sh ~/RetroPie-Setup/scriptmodules/
+		cp -f -R /AmiRock/retropie/retropie_setup.sh ~/RetroPie-Setup/
 
-		sudo chmod -R 777 RetroPie_Rock5b
-
-		cd RetroPie_Rock5b
-
-		./setup.sh  		
+		cd RetroPie-Setup
+		sudo chmod -R 777 /opt
+		sudo __nodialog=1 __platform=rk3588 ./retropie_setup.sh basic_install
+		sudo  ./retropie_setup.sh basic_install 		
 	     ;;
     
          i)
