55b73703188915a720c643fb760c67ca0f66ef56
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2024-10-11 13:25:51 +0200

    Create install.retropie.sh

---
+++ scripts/install.retropie.sh
@@ -0,0 +1,22 @@
+
+    
+    
+    
+    rm -rf ~/RetroPie-Setup
+    clear
+    
+    sudo mkdir -p /etc/emulationstation
+    sudo chmod -R 777 /etc/emulationstation
+    mkdir -p /opt/retropie/emulators
+    sudo chmod -R 777 /opt    
+		sudo apt install -y p7zip-full
+		cd ~
+		git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
+		sudo chmod -R 777 ~/RetroPie-Setup
+		cp -f -R /opt/AmiRock/retropie/system.sh ~/RetroPie-Setup/scriptmodules/
+		cp -f -R /AmiRock/retropie/retropie_setup.sh ~/RetroPie-Setup/
+
+		cd RetroPie-Setup
+		sudo chmod -R 777 /opt
+		sudo __nodialog=1 __platform=rk3588 ./retropie_setup.sh basic_install
+		sudo  ./retropie_setup.sh basic_install
